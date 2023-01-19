2ab8e5b3aa71e94794f9632809023234cc10750b
bambu <bambu@ESSA>
2023-01-19 13:43:26 +0500

    Add project files.

---
+++ BOIAPP/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BOIAPP;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BOIAPP/Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace BOIAPP;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
